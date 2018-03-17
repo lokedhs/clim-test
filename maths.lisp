bfb2daff1b591d98ce8c23107e7d4295cdbd82e3
Elias Mårtenson <lokedhs@gmail.com>
2018-03-17 19:36:44 +0800

    Maths rendering using math-interactor now works

--- maths.lisp
+++ maths.lisp
@@ -7,16 +7,21 @@
 (defun present-to-stream (obj stream)
   (clim:present obj (clim:presentation-type-of obj) :stream stream))
 
-(defclass text-content-view (clim:view)
+(defclass text-content-view (clim:textual-view)
   ())
 
 (defun display-text-content (frame stream)
   (declare (ignore frame))
   (format stream "This is some test content~%")
-  (math-interactor:put-result (polynomials:make-polynomial 10 2 4 8 8)))
+  (let ((expression (polynomials:make-polynomial 10 2 4))
+        (output-record nil))
+    (clim:with-output-recording-options (stream :draw nil)
+      (setf output-record (rtc:render (math-utils-format:format-pretty expression :presentations t)
+                                      stream))
+      (rtc:advance-cursor output-record stream :line-break t))))
 
 (clim:define-application-frame foo-frame ()
-  ()
+  ((math-interactor::formatted-content :initform (list (list 'text-content))))
   (:panes (text-content :application
                         :default-view 'text-content-view
                         :display-function 'display-text-content)
