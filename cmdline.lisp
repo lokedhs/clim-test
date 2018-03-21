630b712177f8f1746c0e1f019cc14bcc79088431
Elias Martenson <lokedhs@gmail.com>
2018-03-21 13:06:18 +0800

    Test code code maxima

---
+++ cmdline.lisp
@@ -0,0 +1,44 @@
+(defpackage :cmdline-test
+  (:use :cl)
+  (:export #:open-foo-frame))
+
+(in-package :cmdline-test)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-package "CLIM")
+    (sb-ext:restrict-compiler-policy 'safety 3)
+    (sb-ext:restrict-compiler-policy 'debug 3)
+    (ql:quickload "mcclim"))
+  (unless (find-package "LOG4CL")
+    (ql:quickload "log4cl")))
+
+(defclass maxima-interactor-view (clim:view)
+  ())
+
+(defclass maxima-interactor-pane (clim:interactor-pane)
+  ())
+
+(defmethod stream-present :around ((stream maxima-interactor-pane) object type
+                                   &rest args
+                                   &key (single-box nil single-box-p) &allow-other-keys)
+  (declare (ignore single-box single-box-p))
+  (apply #'call-next-method stream object type :single-box t args))
+
+(defun print-listener-prompt (stream frame)
+  (declare (ignore frame))
+  (format stream "maxima> "))
+
+(clim:define-application-frame foo-frame ()
+  ()
+  (:panes (text-content (clim:make-clim-stream-pane :type 'maxima-interactor-pane
+                                                    :name 'maxima-interactor
+                                                    :default-view 'maxima-interactor-view))
+          (interaction-pane :interactor))
+  (:top-level (clim:default-frame-top-level :prompt 'print-listener-prompt))
+  (:layouts (default (clim:vertically ()
+                       text-content
+                       interaction-pane))))
+
+(defun open-foo-frame ()
+  (let ((frame (clim:make-application-frame 'foo-frame)))
+    (clim:run-frame-top-level frame)))
