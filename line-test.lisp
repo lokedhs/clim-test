2b5e1f47de9183c73b009fca56fff005d8a516fb
Elias Mårtenson <lokedhs@gmail.com>
2018-04-29 23:45:07 +0800

    Add line-test

---
+++ line-test.lisp
@@ -0,0 +1,76 @@
+(defpackage :line-test
+  (:use :cl)
+  (:export #:open-foo-frame))
+
+(in-package :line-test)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-package "CLIM")
+    (sb-ext:restrict-compiler-policy 'safety 3)
+    (sb-ext:restrict-compiler-policy 'debug 3)
+    (unless (find-package "LOG4CL")
+      (ql:quickload "log4cl"))
+    (ql:quickload "mcclim")))
+
+(declaim (optimize (speed 0) (safety 3) (debug 3)))
+
+(defmacro dimension-bind ((output-record &key
+                                           ((:width width-sym)) ((:height height-sym))
+                                           ((:x x-sym)) ((:y y-sym))
+                                           ((:right right-sym)) ((:bottom bottom-sym))
+                                           ((:baseline baseline-sym)))
+                          &body body)
+  (alexandria:with-gensyms (width height x y)
+    (alexandria:once-only (output-record)
+      (labels ((make-body ()
+                 `(progn ,@body))
+               (make-baseline ()
+                 (if baseline-sym
+                     `(let ((,baseline-sym (clim-extensions:output-record-baseline ,output-record)))
+                        ,(make-body))
+                     (make-body)))
+               (make-position-form ()
+                 (if (or x-sym y-sym right-sym bottom-sym)
+                     `(multiple-value-bind (,x ,y)
+                          (clim:output-record-position ,output-record)
+                        (declare (ignorable ,x ,y))
+                        (let (,@(if x-sym `((,x-sym ,x)))
+                              ,@(if y-sym `((,y-sym ,y)))
+                              ,@(if right-sym `((,right-sym (+ ,x ,width))))
+                              ,@(if bottom-sym `((,bottom-sym (+ ,y ,height)))))
+                          ,(make-baseline)))
+                     (make-baseline))))
+        (if (or width-sym height-sym right-sym bottom-sym)
+            `(multiple-value-bind (,width ,height)
+                 (clim:rectangle-size ,output-record)
+               (declare (ignorable ,width ,height))
+               (let (,@(if width-sym `((,width-sym ,width)))
+                     ,@(if height-sym `((,height-sym ,height))))
+                 ,(make-position-form)))
+            (make-position-form))))))
+
+(defclass text-content-view (clim:view)
+  ())
+
+(clim:define-application-frame foo-frame ()
+  ()
+  (:panes (text-content :application
+                        :default-view 'text-content-view
+                        :display-function 'display-text-content
+                        :redisplay-on-resize-p t)
+          (interaction-pane :interactor))
+  (:layouts (default (clim:vertically ()
+                       text-content
+                       interaction-pane))))
+
+(defun open-foo-frame ()
+  (let ((frame (clim:make-application-frame 'foo-frame :width 700 :height 800)))
+    (clim:run-frame-top-level frame)))
+
+(defun draw-x (stream x y &optional (size 5))
+  (clim:draw-line* stream (- x size) (- y size) (+ x size) (+ y size))
+  (clim:draw-line* stream (- x size) (+ y size) (+ x size) (- y size)))
+
+(defun display-text-content (frame stream)
+  (declare (ignore frame))
+  (clim:draw-line* stream 10 10 150 50 :line-thickness 5))
