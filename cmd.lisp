1251de9c8d7bd75fdd9cc3efca65fe2599175546
Elias Martenson <lokedhs@gmail.com>
2018-05-25 14:59:39 +0800

    More test cases

---
+++ cmd.lisp
@@ -0,0 +1,68 @@
+(defpackage :command-presentation
+  (:use :cl)
+  (:export #:open-foo-frame))
+
+(in-package :command-presentation)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-package "CLIM")
+    (sb-ext:restrict-compiler-policy 'safety 3)
+    (sb-ext:restrict-compiler-policy 'debug 3)
+    (ql:quickload "mcclim"))
+  (unless (find-package "LOG4CL")
+    (ql:quickload "log4cl")))
+
+(defun present-to-stream (obj stream)
+  (clim:present obj (clim:presentation-type-of obj) :stream stream))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defclass foo ()
+    ((value :type string
+            :initarg :value
+            :initform "foo"
+            :reader foo/value))))
+
+(clim:define-command-table foo-commands)
+
+(defun display-text-content (frame stream)
+  (format stream "Some content~%")
+  (dolist (obj (foo-frame/objects frame))
+    (present-to-stream obj stream)))
+
+(clim:define-application-frame foo-frame ()
+  ((values :initform nil
+            :accessor foo-frame/objects))
+  (:panes (text-content :application
+                        :display-function 'display-text-content)
+          (interaction-pane :interactor))
+  (:command-table (foo-frame :inherit-from (foo-commands)))
+  (:layouts (default (clim:vertically ()
+                       text-content
+                       interaction-pane))))
+
+(defun open-foo-frame ()
+  (let ((frame (clim:make-application-frame 'foo-frame
+                                            :width 600
+                                            :height 600)))
+    (clim:run-frame-top-level frame)))
+
+(clim:define-presentation-translator foo-to-string (string foo foo-commands)
+    (object)
+  (log:info "Calling command translator for: ~s" object)
+  (make-instance 'foo :value object))
+
+(clim:define-presentation-method clim:present (obj (type foo) stream (view t) &key)
+  (let ((value (foo/value obj)))
+    (clim:with-room-for-graphics (stream :first-quadrant nil)
+      (clim:draw-text* stream value 0 0)
+      (clim:draw-line* stream 0 0 50 0))))
+
+(clim:define-command (show-object-command :name "Show object" :menu t :command-table foo-commands)
+    ((obj 'foo :prompt "Object")
+     (text 'string :prompt "Text"))
+  (format *standard-output* "Text is: ~a~%" text)
+  (push obj (foo-frame/objects clim:*application-frame*)))
+
+(clim:define-command (show-string-object-command :name "Show string object" :menu t :command-table foo-commands)
+    ((value 'string :prompt "Value"))
+  (push (make-instance 'foo :value value) (foo-frame/objects clim:*application-frame*)))
--- performance.lisp
+++ performance.lisp
@@ -1,36 +1,38 @@
-(defpackage :simple-text
+(defpackage :perf-test
   (:use :cl)
   (:export #:open-foo-frame))
 
-(in-package :simple-text)
-
-(declaim (optimize (speed 0) (safety 3) (debug 3)))
+(in-package :perf-test)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-package "LOG4CL")
+    (ql:quickload "log4cl"))
   (unless (find-package "CLIM")
     #+sbcl
     (progn
       (sb-ext:restrict-compiler-policy 'safety 3)
       (sb-ext:restrict-compiler-policy 'debug 3))
-    (ql:quickload "mcclim"))
-  (unless (find-package "LOG4CL")
-    (ql:quickload "log4cl")))
+    (ql:quickload "mcclim")))
 
-(defun display-text-content (frame stream)
-  (declare (ignore frame)) 
-  (loop
-    for i from 0 below 200
-    do (format stream "Line number ~d: Some more text here: ~r. And add even more text here to make drawing slower.~%" i i)))
+(defun present-to-stream (obj stream)
+  (clim:present obj (clim:presentation-type-of obj) :stream stream))
 
-(clim:define-application-frame foo-frame ()
+(clim:define-application-frame main-frame ()
   ()
   (:panes (text-content :application
                         :display-function 'display-text-content))
   (:layouts (default (clim:vertically ()
                        text-content))))
 
+
+(defun display-text-content (frame stream)
+  (declare (ignore frame))
+  (loop
+    for x from 0 to 200
+    do (princ (format nil "Line with number ~s: Some text ~r. Test foo test. Test foo test. Test foo test.~%" x x) stream)))
+
 (defun open-foo-frame ()
-  (let ((frame (clim:make-application-frame 'foo-frame
+  (let ((frame (clim:make-application-frame 'main-frame
                                             :width 800
                                             :height 800)))
     (clim:run-frame-top-level frame)))
