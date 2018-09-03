e0098cb1c29f08a7c62a9a82ea1fc6020089291e
Elias Mårtenson <lokedhs@gmail.com>
2018-09-03 18:49:53 +0800

    Copy&paste test

--- performance.lisp
+++ performance.lisp
@@ -14,26 +14,40 @@
       (sb-ext:restrict-compiler-policy 'debug 3))
     (ql:quickload "mcclim")))
 
+(clim:define-presentation-type html-text ())
+
+(clim:define-presentation-method clim-internals::convert-clipboard-content
+    (obj (type html-text) (output-type (eql :string)) check-only)
+  obj)
+
+(clim:define-presentation-method clim-internals::convert-clipboard-content
+    (obj (type html-text) (output-type (eql :html)) check-only)
+  obj)
+
 (defun present-to-stream (obj stream)
   (clim:present obj (clim:presentation-type-of obj) :stream stream))
 
 (clim:define-application-frame main-frame ()
   ()
   (:panes (text-content :application
-                        :display-function 'display-text-content))
+                        :display-function 'display-text-content)
+          (interactor :interactor))
   (:layouts (default (clim:vertically ()
-                       text-content))))
+                       text-content
+                       interactor))))
 
 
 (defun display-text-content (frame stream)
   (declare (ignore frame))
   (let ((rec (clim:with-output-to-output-record (stream)
                (loop
-                 for x from 0 to 200
+                 for x from 0 below 20
                  do (format stream "Line with number ~s: Some text ~r. Test foo test. Test foo test. Test foo test.~%" x x)))))
     (clim:with-room-for-graphics (stream)
       (clim:stream-add-output-record stream rec)))
-  (format stream "hello~%"))
+  (clim:present "Plain string" 'string :stream stream)
+  (format stream "~%")
+  (clim:present "Some <b>html text</b>" 'html-text :stream stream))
 
 (defun open-foo-frame ()
   (let ((frame (clim:make-application-frame 'main-frame
