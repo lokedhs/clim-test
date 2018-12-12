49b5a835cea93bc415d2458dfb3d78f3494409d2
Elias Mårtenson <lokedhs@gmail.com>
2018-12-13 00:35:47 +0800

    Some more experiments with more windows

--- win.lisp
+++ win.lisp
@@ -11,24 +11,33 @@
 
 (in-package :clim-test.window)
 
+(clim:define-presentation-type foo-type ())
+
+(clim:define-presentation-method clim:present (obj (type foo-type) stream view &key)
+  (format stream "Foo-type: ~a" obj))
+
 (clim:define-application-frame foo-frame ()
   ()
   (:panes (documentation-content :application
                                  :display-function 'display-content
                                  :display-time t
                                  :incremental-redisplay t)
+          (main-input (clim:make-pane 'clim:text-field-pane :name 'main-input))
           (interaction-pane :interactor))
   (:layouts (default (clim:vertically ()
                        documentation-content
+                       main-input
                        interaction-pane))))
 
 (defun display-content (frame stream)
   (declare (ignore frame))
-  (format stream "Test content"))
+  (format stream "Test content~%")
+  (clim:stream-present stream "main-frame" 'foo-type))
 
 (defun display-popup-content (frame stream)
   (declare (ignore frame))
-  (format stream "This is the content of the popup"))
+  (format stream "This is the content of the popup~%")
+  (clim:stream-present stream "popup-frame" 'foo-type))
 
 (defun open-foo-frame ()
   (let ((frame (clim:make-application-frame 'foo-frame)))
@@ -36,10 +45,12 @@
 
 (clim:define-application-frame maxima-documentation-frame ()
   ()
-  (:panes (content :application
-                   :display-function 'display-popup-content))
+  (:panes (popup-content :application
+                         :display-function 'display-popup-content)
+          (doc-input (clim:make-pane 'clim:text-field-pane :name 'doc-input)))
   (:layouts (default (clim:vertically ()
-                       content))))
+                       doc-input
+                       popup-content))))
 
 (define-foo-frame-command (open-window-command :name "Open Window")
     ()
@@ -47,17 +58,35 @@
          (frame (clim:make-application-frame 'maxima-documentation-frame
                                              :frame-manager fm
                                              :width 400
-                                             :height 400)))
-    (clim:adopt-frame fm frame)
-    (clim:enable-frame frame)
-    (let ((pane (clim:find-pane-named frame 'documentation-content)))
-      (assert (not (null pane)))
-      ;; This is just a test loop to see if the window updates at all.
-      ;; Currently, it does not. But the error doesn't seem to be
-      ;; directly related to the event loop. Instead, it seems to crash
-      ;; because there is something wrong with the setup of the panes
-      ;; in the frame.
-      (loop
-        named control-loop
-        for gesture = (clim:read-gesture :stream pane)
-        do (log:info "gesture = ~s" gesture)))))
+                                             :height 400
+                                             :event-queue (climi::frame-event-queue clim:*application-frame*))))
+    #+nil
+    (bordeaux-threads:make-thread (lambda () (clim:run-frame-top-level frame)))
+    (progn
+      #+nil (clim:adopt-frame fm frame)
+      (log:info "q = ~s" (climi::frame-event-queue clim:*application-frame*))
+      #+nil (push frame (slot-value fm 'climi::frames))
+      (let ((toplevel (clim:frame-top-level-sheet frame))
+            (graft (clim:find-graft :port (clim:port fm))))
+        (assert (not (null toplevel)))
+        (setf (slot-value toplevel 'climi::frame) clim:*application-frame*)
+        (clim:sheet-adopt-child graft toplevel)
+        #+nil (setf (clim:graft frame) graft))
+      (clim:enable-frame frame)
+      (let ((pane (clim:find-pane-named clim:*application-frame* 'documentation-content)))
+        (log:info "pane = ~s" pane)
+        #+nil (assert (not (null pane)))
+        ;; This is just a test loop to see if the window updates at all.
+        ;; Currently, it does not. But the error doesn't seem to be
+        ;; directly related to the event loop. Instead, it seems to crash
+        ;; because there is something wrong with the setup of the panes
+        ;; in the frame.
+        #+nil
+        (loop
+          named control-loop
+          for gesture = (clim:read-gesture :stream pane)
+          do (log:info "gesture = ~s" gesture))))))
+
+(define-foo-frame-command (show-foo-type :name "Show")
+    ((obj foo-type :prompt "Select instance"))
+  (log:info "Selected objected: ~s" obj))
