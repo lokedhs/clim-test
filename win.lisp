67942b4c6bb85a54251b465a0b38da55d410f66f
Elias Mårtenson <lokedhs@gmail.com>
2018-12-13 18:56:25 +0800

    Windowing test code can now open windows in multiple threads

--- win.lisp
+++ win.lisp
@@ -60,33 +60,38 @@
                                              :width 400
                                              :height 400
                                              :event-queue (climi::frame-event-queue clim:*application-frame*))))
-    #+nil
-    (bordeaux-threads:make-thread (lambda () (clim:run-frame-top-level frame)))
-    (progn
-      #+nil (clim:adopt-frame fm frame)
-      (log:info "q = ~s" (climi::frame-event-queue clim:*application-frame*))
-      #+nil (push frame (slot-value fm 'climi::frames))
-      (let ((toplevel (clim:frame-top-level-sheet frame))
-            (graft (clim:find-graft :port (clim:port fm))))
-        (assert (not (null toplevel)))
-        (setf (slot-value toplevel 'climi::frame) clim:*application-frame*)
-        (clim:sheet-adopt-child graft toplevel)
-        #+nil (setf (clim:graft frame) graft))
-      (clim:enable-frame frame)
-      (let ((pane (clim:find-pane-named clim:*application-frame* 'documentation-content)))
-        (log:info "pane = ~s" pane)
-        #+nil (assert (not (null pane)))
-        ;; This is just a test loop to see if the window updates at all.
-        ;; Currently, it does not. But the error doesn't seem to be
-        ;; directly related to the event loop. Instead, it seems to crash
-        ;; because there is something wrong with the setup of the panes
-        ;; in the frame.
-        #+nil
-        (loop
-          named control-loop
-          for gesture = (clim:read-gesture :stream pane)
-          do (log:info "gesture = ~s" gesture))))))
+    #+nil (clim:adopt-frame fm frame)
+    (log:info "q = ~s" (climi::frame-event-queue clim:*application-frame*))
+    #+nil (push frame (slot-value fm 'climi::frames))
+    (let ((toplevel (clim:frame-top-level-sheet frame))
+          (graft (clim:find-graft :port (clim:port fm))))
+      (assert (not (null toplevel)))
+      (setf (slot-value toplevel 'climi::frame) clim:*application-frame*)
+      (clim:sheet-adopt-child graft toplevel)
+      #+nil (setf (clim:graft frame) graft))
+    (clim:enable-frame frame)
+    (let ((pane (clim:find-pane-named clim:*application-frame* 'documentation-content)))
+      (log:info "pane = ~s" pane)
+      #+nil (assert (not (null pane)))
+      ;; This is just a test loop to see if the window updates at all.
+      ;; Currently, it does not. But the error doesn't seem to be
+      ;; directly related to the event loop. Instead, it seems to crash
+      ;; because there is something wrong with the setup of the panes
+      ;; in the frame.
+      #+nil
+      (loop
+        named control-loop
+        for gesture = (clim:read-gesture :stream pane)
+        do (log:info "gesture = ~s" gesture)))))
 
 (define-foo-frame-command (show-foo-type :name "Show")
     ((obj foo-type :prompt "Select instance"))
   (log:info "Selected objected: ~s" obj))
+
+(define-foo-frame-command (toplevel-application-pane-command :name "Toplevel")
+    ()
+  (let ((frame (clim:make-application-frame 'maxima-documentation-frame
+                                            :width 400
+                                            :height 400)))
+    (bordeaux-threads:make-thread (lambda ()
+                                    (clim:run-frame-top-level frame)))))
