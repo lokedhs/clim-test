031d943c51b734908338d062972cd3d1706f2eba
Elias Mårtenson <lokedhs@gmail.com>
2018-12-13 11:48:48 +0800

    More experiments with word wrap

--- word-wrap-new.lisp
+++ word-wrap-new.lisp
@@ -43,6 +43,18 @@
                  ,(make-position-form)))
             (make-position-form))))))
 
+(defun set-rec-position (output-record x y)
+  (dimension-bind (output-record :x old-x :y old-y)
+    (setf (clim:output-record-position output-record)
+          (values (or x old-x)
+                  (or y old-y)))))
+
+(defun move-rec (output-record dx dy)
+  (dimension-bind (output-record :x old-x :y old-y)
+    (setf (clim:output-record-position output-record)
+          (values (+ dx old-x)
+                  (+ dy old-y)))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Word-wrap implementation
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -72,9 +84,10 @@
                 (return (reverse result))))))
 
 (defvar *word-wrap-x* nil)
+(defvar *word-wrap-y* nil)
+(defvar *word-wrap-line-content* nil)
 (defvar *word-wrap-right-margin* nil)
 (defvar *word-wrap-height* nil)
-(defvar *word-wrap-line* nil)
 
 (defun font-height (stream)
   (multiple-value-bind (width ascent descent left right font-ascent font-descent)
@@ -85,13 +98,36 @@
     (log:info "a=~s d=~s" font-ascent font-descent)
     (+ font-ascent font-descent)))
 
+(defun draw-current-line (stream)
+  (log:info "drawing line. height=~s. Content: ~s" *word-wrap-height* (map 'list (lambda (rec)
+                                                                                   (dimension-bind (rec :y y)
+                                                                                     (- y)))
+                                                                           *word-wrap-line-content*))
+  (let ((height (max *word-wrap-height*
+                     (reduce #'max
+                             *word-wrap-line-content*
+                             :key (lambda (rec)
+                                    (dimension-bind (rec :y y)
+                                      (- y)))))))
+    (loop
+      for rec across *word-wrap-line-content*
+      do (multiple-value-bind (x y)
+             (clim:output-record-position rec)
+           (declare (ignore y))
+           (setf (clim:output-record-position rec) (values x *word-wrap-y*))
+           (clim:stream-add-output-record stream rec)))
+    (incf *word-wrap-y* height)))
+
 (defmacro with-word-wrap ((stream &key right-margin height) &body body)
   (alexandria:once-only (stream right-margin height)
     `(let ((*word-wrap-x* 0)
+           (*word-wrap-y* 0)
            (*word-wrap-right-margin* (or ,right-margin (clim:rectangle-width (clim:pane-viewport-region ,stream))))
-           (*word-wrap-height* (or ,height (font-height stream))))
-       (let ((*word-wrap-line* *word-wrap-height*))
-         ,@body))))
+           (*word-wrap-height* (or ,height (font-height stream)))
+           (*word-wrap-line-content* (make-array 0 :adjustable t :fill-pointer t)))
+       (prog1
+           (progn ,@body)
+         (draw-current-line ,stream)))))
 
 (defun split-string-at-right-margin (stream parts right-margin)
   (loop
@@ -112,15 +148,17 @@
 
 (defun word-wrap-draw-one-line (stream parts)
   (let ((start *word-wrap-x*)
-        (right-margin *word-wrap-right-margin*)
-        (line *word-wrap-line*))
+        (right-margin *word-wrap-right-margin*))
     (multiple-value-bind (initial more-parts width)
         (split-string-at-right-margin stream parts (- right-margin start))
       (log:info "drawing ~s at ~s" initial start)
-      (clim:draw-text* stream initial start line)
+      (let ((rec (clim:with-output-to-output-record (stream)
+                   (clim:draw-text* stream initial start 0))))
+        (vector-push-extend rec *word-wrap-line-content*))
       (cond (more-parts
-             (setq *word-wrap-x* 0)
-             (incf *word-wrap-line* *word-wrap-height*))
+             (draw-current-line stream)
+             (setq *word-wrap-line-content* (make-array 0 :adjustable t :fill-pointer t))
+             (setq *word-wrap-x* 0))
             (t
              (incf *word-wrap-x* width)))
       more-parts)))
@@ -168,9 +206,10 @@
       (dolist (s content)
         (append-word-wrap-content coll s)))
     (when content
-      (with-word-wrap (stream)
-        (dolist (s content)
-          (word-wrap-draw-string stream s))))))
+      (clim:with-room-for-graphics (stream :first-quadrant nil)
+        (with-word-wrap (stream)
+          (dolist (s content)
+            (word-wrap-draw-string stream s)))))))
 
 (define-foo-frame-command (add-string :name "Add")
     ((text 'string))
