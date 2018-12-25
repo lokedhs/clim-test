09fe99edf013a86714e21c112ac9256159d67c36
Elias Mårtenson <lokedhs@gmail.com>
2018-12-25 15:11:49 +0800

    Made the demonstration of the unexpected behaviour better

--- image.lisp
+++ image.lisp
@@ -25,18 +25,22 @@
 (defun display-content (frame stream)
   (declare (ignore frame))
   (let ((image (clim:make-pattern-from-bitmap-file "plotting1.png")))
-    (labels ((make-rec ()
+    (labels ((make-rec (title)
                (clim:with-output-to-output-record (stream)
                  (clim:with-room-for-graphics (stream :first-quadrant nil)
-                   (clim:draw-pattern* stream image 0 0)))))
-      (let ((rec (make-rec)))
+                   (clim:draw-rectangle* stream 0 0 (clim:pattern-width image) (clim:pattern-height image) :ink image)
+                   (clim:draw-text* stream title (+ 50 (clim:pattern-width image)) 100)))))
+      (let ((rec (make-rec "Position 10")))
         (setf (clim:output-record-position rec) (values 10 10))
         (clim:stream-add-output-record stream rec))
-      (let ((rec (make-rec)))
+      (let ((rec (make-rec "Position 400.5")))
         ;; Note the additional half-pixel in the y-coordinate. If the
         ;; y-coordinate is an integer, then everything works
         ;; correctly.
         (setf (clim:output-record-position rec) (values 10 400.5))
+        (clim:stream-add-output-record stream rec))
+      (let ((rec (make-rec "Position 800.49999")))
+        (setf (clim:output-record-position rec) (values 10 800.49999))
         (clim:stream-add-output-record stream rec)))))
 
 (defun open-foo-frame ()
