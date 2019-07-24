a8a262cc8f7a36751465d13aa2f67afabfe02880
Elias Mårtenson <lokedhs@gmail.com>
2019-07-24 17:23:38 +0800

    Clean up the code a bit

--- line-test.lisp
+++ line-test.lisp
@@ -14,41 +14,6 @@
 
 (declaim (optimize (speed 0) (safety 3) (debug 3)))
 
-(defmacro dimension-bind ((output-record &key
-                                           ((:width width-sym)) ((:height height-sym))
-                                           ((:x x-sym)) ((:y y-sym))
-                                           ((:right right-sym)) ((:bottom bottom-sym))
-                                           ((:baseline baseline-sym)))
-                          &body body)
-  (alexandria:with-gensyms (width height x y)
-    (alexandria:once-only (output-record)
-      (labels ((make-body ()
-                 `(progn ,@body))
-               (make-baseline ()
-                 (if baseline-sym
-                     `(let ((,baseline-sym (clim-extensions:output-record-baseline ,output-record)))
-                        ,(make-body))
-                     (make-body)))
-               (make-position-form ()
-                 (if (or x-sym y-sym right-sym bottom-sym)
-                     `(multiple-value-bind (,x ,y)
-                          (clim:output-record-position ,output-record)
-                        (declare (ignorable ,x ,y))
-                        (let (,@(if x-sym `((,x-sym ,x)))
-                              ,@(if y-sym `((,y-sym ,y)))
-                              ,@(if right-sym `((,right-sym (+ ,x ,width))))
-                              ,@(if bottom-sym `((,bottom-sym (+ ,y ,height)))))
-                          ,(make-baseline)))
-                     (make-baseline))))
-        (if (or width-sym height-sym right-sym bottom-sym)
-            `(multiple-value-bind (,width ,height)
-                 (clim:rectangle-size ,output-record)
-               (declare (ignorable ,width ,height))
-               (let (,@(if width-sym `((,width-sym ,width)))
-                     ,@(if height-sym `((,height-sym ,height))))
-                 ,(make-position-form)))
-            (make-position-form))))))
-
 (defclass text-content-view (clim:view)
   ())
 
@@ -56,8 +21,7 @@
   ()
   (:panes (text-content :application
                         :default-view 'text-content-view
-                        :display-function 'display-text-content
-                        :redisplay-on-resize-p t))
+                        :display-function 'display-text-content))
   (:layouts (default (clim:vertically ()
                        text-content))))
 
